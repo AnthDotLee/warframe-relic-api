b4178bc76c64bb05e1234b5002f4969eefbcacaf
Anthony Lee <anthony.mi.lee@outlook.com>
2019-02-20 13:30:56 -0600

    Added stubs for findRelic operations

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -9,13 +10,25 @@ func main() {
 
 	router := gin.Default()
 
-	relicsApi := router.Group("relics")
+	relicsApi := router.Group("/relics")
 	{
 		relicsApi.GET("/", func(c *gin.Context) {
 			c.Header("Content-Type", "application/json")
 			c.JSON(http.StatusOK, getRelics())
 		})
 	}
+	relicSearchApi := router.Group("/relic")
+	{
+		relicSearchApi.GET("/:era/:name", relicSearchHandler)
+	}
 
-	router.Run(":3000")
+	err := router.Run(":3000")
+
+	if err != nil {
+		fmt.Printf("Server was unable to start do to error: %s", err)
+	}
+}
+
+func relicSearchHandler(c *gin.Context) {
+
 }
--- relics.go
+++ relics.go
@@ -1,47 +1,52 @@
 package main
 
+var Relics = []Relic{
+	{
+		"Lith",
+		"A1",
+		RelicRewards{
+			[]string{
+				"Braton Prime Barrel",
+				"Forma Blueprint",
+				"Vasto Prime Receiver",
+			},
+			[]string{
+				"Saryn Prime Neuroptics Blueprint",
+				"Vectis Prime Blueprint",
+			},
+			[]string{
+				"Akstiletto Prime Blueprint",
+			},
+		},
+		true,
+	},
+	{
+		"Lith",
+		"A2",
+		RelicRewards{
+			[]string{
+				"Lex Prime Barrel",
+				"Forma Blueprint",
+				"Valkyr Prime Blueprint",
+			},
+			[]string{
+				"Akbronco Prime Link",
+				"Cernos Prime Blueprint",
+			},
+			[]string{
+				"Akstiletto Prime Blueprint",
+			},
+		},
+		true,
+	},
+}
+
 func getRelics() []Relic {
-
-	var Relics = []Relic{
-		{
-			"Lith",
-			"A1",
-			RelicRewards{
-				[]string{
-					"Braton Prime Barrel",
-					"Forma Blueprint",
-					"Vasto Prime Receiver",
-				},
-				[]string{
-					"Saryn Prime Neuroptics Blueprint",
-					"Vectis Prime Blueprint",
-				},
-				[]string{
-					"Akstiletto Prime Blueprint",
-				},
-			},
-			true,
-		},
-		{
-			"Lith",
-			"A2",
-			RelicRewards{
-				[]string{
-					"Lex Prime Barrel",
-					"Forma Blueprint",
-					"Valkyr Prime Blueprint",
-				},
-				[]string{
-					"Akbronco Prime Link",
-					"Cernos Prime Blueprint",
-				},
-				[]string{
-					"Akstiletto Prime Blueprint",
-				},
-			},
-			true,
-		},
-	}
-
 	return Relics
+}
+
+func findRelic(era string, name string) Relic {
+	for i := 0; i < len(Relics); i++ {
+
+	}
 }
